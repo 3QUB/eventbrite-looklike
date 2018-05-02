4a9493cdcb2743b28de974b6394f0621154d4b74
Quentin <berson.quentin@gmail.com>
2018-05-02 15:13:40 +0200

    ajout loggin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   resources :user
+  get    '/secret',   to: 'static_pages#secret'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end

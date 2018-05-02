0aa57f08f5b1b824172e9ed50172c9bdff07ad2a
Quentin <berson.quentin@gmail.com>
2018-05-02 16:02:26 +0200

    add log_in methode to create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: params[:session][:email].downcase)
-    if @user && @user.authenticate(params[:session][:password])
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
       redirect_to secret_path
       # Log the user in and redirect to the user's show page.
     else

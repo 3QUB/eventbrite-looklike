9eeb490b01359c9b2e84d569094bb3a01a237d0d
Quentin <berson.quentin@gmail.com>
2018-05-02 13:49:43 +0200

    add create and show

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,9 +4,13 @@ class UserController < ApplicationController
   end
 
   def create
+    @user = User.new(name: params["name"], email: params["email"], password: params["password"], password_confirmation: params["password_confirmation"])
+    @user.save
+    redirect_to user_path(@user)
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit

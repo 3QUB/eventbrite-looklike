ca76926b1f66b22517797e8772148e312eefeffc
Quentin <berson.quentin@gmail.com>
2018-05-03 18:41:07 +0200

    ijakuhjzbeb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   resources :user
-  resources :events
+  resources :events do
+    post '/subscribe', to: 'events#subscribe'
+  end
   get    '/login',                  to: 'sessions#new'
   post   '/login',                  to: 'sessions#create'
   delete '/logout',                 to: 'sessions#destroy'
-  post   '/events/:id/subscribe',   to: 'events#subscribe'
+  #post   '/events/:id/subscribe',   to: 'events#subscribe'
 end

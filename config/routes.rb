1bde1efb9302e5a3b9cec8a226fd9b0152800d39
Quentin <berson.quentin@gmail.com>
2018-05-03 14:42:36 +0200

    changement formulaire sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  get 'events/new'
-  get 'events/show'
-  get 'events/edit'
-  get 'events/index'
   root 'static_pages#home'
   resources :user
-  get    '/secret',  to: 'static_pages#secret'
+  resources :events
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'

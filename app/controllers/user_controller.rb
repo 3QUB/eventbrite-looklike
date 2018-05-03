40efb121a16c7b7e380ffda3c8cf3219b7d02bf7
Quentin <berson.quentin@gmail.com>
2018-05-03 14:51:01 +0200

    test fonctionnement form sign up

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,7 +4,8 @@ class UserController < ApplicationController
   end
 
   def create
-    @user = User.new(name: params["name"], email: params["email"], password: params["password"], password_confirmation: params["password_confirmation"])
+    #@user = User.new(name: params["name"], email: params["email"], password: params["password"], password_confirmation: params["password_confirmation"])
+    @user = User.new(user_params)
     if @user.save
       log_in @user
       flash[:success] = "Welcome to the Sample App!"
@@ -28,6 +29,12 @@ class UserController < ApplicationController
   end
 
   def index
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
   
 end
